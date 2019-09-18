dad13dad47349e8c9363b026d3898deb14659f73
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-18 12:39:29 +0100

    set up project and add initiaalTeamList

---
+++ team-builder/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const initialTeamList = [
+  {
+  name: '',
+  id: '',
+  email: '',
+  role: '',
+}
+] 
+
+function App() {
+
+const [teamList, setTeamList] = useState(initialTeamList);
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p key={teamList.id}> 
+          Name: {teamList.name}
+          Email: {teamList.email}
+          Role: {teamList.role}
+        </p>
+  {/*       <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
