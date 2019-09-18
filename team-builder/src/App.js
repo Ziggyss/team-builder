ce2119efbe12dd65cc03219616b3b4871915ef83
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-18 12:51:46 +0100

    build initial form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const initialTeamList = [
   {
-  name: '',
-  id: '',
-  email: '',
-  role: '',
-}
-] 
+    name: "",
+    id: "",
+    email: "",
+    role: ""
+  }
+];
 
 function App() {
-
-const [teamList, setTeamList] = useState(initialTeamList);
-
+  const [teamList, setTeamList] = useState(initialTeamList);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p key={teamList.id}> 
+        <p key={teamList.id}>
           Name: {teamList.name}
           Email: {teamList.email}
           Role: {teamList.role}
         </p>
-  {/*       <a
+        {/*       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
---
+++ team-builder/src/Form.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form>
+      <label htmlFor="nameInput">Name:</label>
+      <input value={name} id="nameInput" type="text"></input>
+
+      <label htmlFor="emailInput">Email:</label>
+      <input value={email} id="emailInput" type="text"></input>
+
+      <label htmlFor="roleInput">Role:</label>
+      <input value={role} id="roleInput" type="text"></input>
+
+      <button>
+          Submit!
+      </button>
+    </form>
+  );
+}
