721fb1316f63494211948d6b9668427fde72f24b
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-18 13:34:25 +0100

    feed the data change functions to the form as props

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,23 +1,35 @@
 import React from "react";
 
-
-
-
 function Form() {
+  const { onNameChange, onEmailChange, onRoleChange } = props;
+
   return (
     <form>
       <label htmlFor="nameInput">Name:</label>
-      <input value={name} id="nameInput" type="text"></input>
+      <input
+        value={name}
+        id="nameInput"
+        type="text"
+        onChange={onNameChange}
+      ></input>
 
       <label htmlFor="emailInput">Email:</label>
-      <input value={email} id="emailInput" type="text"></input>
+      <input
+        value={email}
+        id="emailInput"
+        type="text"
+        onChange={onEmailChange}
+      ></input>
 
       <label htmlFor="roleInput">Role:</label>
-      <input value={role} id="roleInput" type="text"></input>
+      <input
+        value={role}
+        id="roleInput"
+        type="text"
+        onChange={onRoleChange}
+      ></input>
 
-      <button>
-          Submit!
-      </button>
+      <button>Submit!</button>
     </form>
   );
 }
